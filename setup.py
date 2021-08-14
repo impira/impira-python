bd82f3a87db99381b5d8b5fcd2693e0830efcde6
Ankur Goyal <ankrgyl@gmail.com>
2021-08-14 15:30:34 -0700

    Increment version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="impira",
-    version="0.0.1",
+    version="0.0.2",
     author="Impira Engineering",
     author_email="engineering@impira.com",
     description="Official Impira Python SDK",
