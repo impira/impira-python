1a3c3e862c71fb188299c55eee3f3e235b6f3dc4
Gustavo Angulo <gangulo1996@gmail.com>
2022-03-16 21:13:31 +0000

    Add train input query benchmarks

--- examples/benchmark.py
+++ examples/benchmark.py
@@ -17,6 +17,9 @@ SEED = 333
 
 ZERO_CURSOR = "0200000000000000000000"
 
+TABLE_TRAINER = "entity_one_many"
+TEXT_TRAINER = "text_string-dev-1"
+
 
 def pagination_query(api: impira.Impira, collection_id: str):
     query = "@`file_collections::%s`[uid]" % (collection_id)
@@ -60,43 +63,89 @@ def ie_collection_query(api: impira.Impira, collection_id: str):
     api.query(query=query, mode="poll", cursor=ZERO_CURSOR, timeout=0)
 
 
-# def _get_inferred_fields(api: impira.Impira, collection_id: str):
-#     query = """
-#     @`__system::ecs`[id, fields: (fields[id, name: field.name, template: field_template, trainer: infer_func.trainer_name])] name="file_collections::%s"
-#     [.:flatten(merge_unnest(build_entity("ec_id", id), fields))]
-#     [.] template="inferred_field_spec"
-#     """ % (collection_id)
-#     result = api.query(query)
-#     return result
+def _get_inferred_fields(api: impira.Impira, collection_id: str):
+    query = """
+    @`__system::ecs`[id, fields: (fields[id, name: field.name, template: field_template, trainer: infer_func.trainer_name])] name="file_collections::%s"
+    [.:flatten(merge_unnest(build_entity("ec_id", id), fields))]
+    [.] template="inferred_field_spec"
+    """ % (collection_id)
+    result = api.query(query)
+    return result["data"]
 
 
-# def _get_field_info_with_trainer(api: impira.Impira, collection_id: str, trainer: str):
-#     inferred_fields = _get_inferred_fields(api, collection_id)
-#     ret = None
-#     for field in inferred_fields:
-#         if field["trainer"] == trainer:
-#             ret = field
+def _get_field_info_with_trainer(api: impira.Impira, collection_id: str, trainer: str):
+    inferred_fields = _get_inferred_fields(api, collection_id)
+    ret = None
+    for field in inferred_fields:
+        if field["trainer"] == trainer:
+            ret = field
 
-#     assert ret != None, "Could not find field with trainer: %s" % trainer
-#     return ret
-
-# def get_table_field_info(api: impira.Impira, collection_id: str):
-#     return _get_field_info_with_trainer(api, collection_id, "text_number-dev-1")
+    assert ret != None, "Could not find field with trainer: %s" % trainer
+    return ret
 
 
-# def get_text_field_info(api: impira.Impira, collection_id: str):
-#     return _get_field_info_with_trainer(api, collection_id, "entity_one_many")
+def get_table_field_info(api: impira.Impira, collection_id: str):
+    return _get_field_info_with_trainer(api, collection_id, TABLE_TRAINER)
+
+
+def get_text_field_info(api: impira.Impira, collection_id: str):
+    return _get_field_info_with_trainer(api, collection_id, TEXT_TRAINER)
+
+
+def escape(input: str):
+    return "`{}`".format(input)
+
+
+def _prepare_train_input_field_query(api: impira.Impira, org_id: int, collection_id: str, trainer: str, gz: bool):
+    field_info = _get_field_info_with_trainer(api, collection_id, trainer)
+    storage_ec = "forge::{}::{}".format(org_id, field_info["ec_id"])
+    storage_name = "f%d" % field_info["id"]
+    field_name = field_info["name"]
+
+    projections = ["uid", "file_id: File.file_id"]
+
+    if gz:
+        field_heap = "{}::fixed_schema_normalized::stitched::{}".format(
+            storage_ec, storage_name)
+        join = "{0}: join_one({1}, uid, uid).{0}".format(
+            escape(field_name), field_heap)
+        projections.append(join)
+        projections.append("user_tag: {}".format(escape(field_name)))
+
+    else:
+        field_heap = "{}::fields::{}".format(storage_ec, storage_name)
+        if trainer == TABLE_TRAINER:
+            field_heap += "::stitched"
+
+        join = "{0}: join_one({1}, uid, uid).{0}[raw_user_tag_v2]".format(
+            storage_name, field_heap)
+
+        projections.append(join)
+        projections.append("user_tag: {}.raw_user_tag_v2".format(storage_name))
+
+    assert field_heap != None
+
+    query = "@`{}::data`[{}]".format(storage_ec, ", ".join(projections))
+
+    return [api, query]
+
+
+def train_input_text_field_query(api: impira.Impira, query: str):
+    api.query(query=query, mode="poll", cursor=ZERO_CURSOR, timeout=0)
 
 
 def benchmark_function(function, prepare_fn, num_runs, args):
     total_time = 0
     for _ in range(num_runs):
-        start = time.time()
+        # Prepare arguments before timing the function
         arguments = args
         if prepare_fn != None:
             arguments = prepare_fn(*args)
+
+        start = time.time()
         function(*arguments)
         end = time.time()
+
         delta = end - start
         total_time += (delta * 1000)
 
@@ -106,9 +155,11 @@ def benchmark_function(function, prepare_fn, num_runs, args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--org-name", type=str, default=None, required=True)
+    parser.add_argument("--org-id", type=int, default=None, required=True)
     parser.add_argument("--api-token", type=str, default=None, required=True)
     parser.add_argument("--collection", type=str, default=None)
     parser.add_argument("--base-url", type=str, default=None, required=True)
+    parser.add_argument("--gz", default=False, action="store_true")
     parser.add_argument(
         "--parallelism", default=multiprocessing.cpu_count(), type=int)
 
@@ -125,7 +176,7 @@ if __name__ == "__main__":
     impira_api = impira.Impira(
         org_name=args.org_name, api_token=args.api_token, base_url=args.base_url)
 
-    # Create the collection if not specified by user
+  # Create the collection if not specified by user
     collection_id = args.collection
     if args.collection is None:
         name = "Benchmark-" + str(time.time_ns() // 1000)
@@ -177,6 +228,18 @@ if __name__ == "__main__":
             "function": ie_collection_query,
             "args": [impira_api, collection_id]
         },
+        {
+            "name": "Train Input Text Field Query",
+            "prepare_fn": _prepare_train_input_field_query,
+            "function": train_input_text_field_query,
+            "args": [impira_api, args.org_id, collection_id, TEXT_TRAINER, args.gz]
+        },
+        {
+            "name": "Train Input Table Field Query",
+            "prepare_fn": _prepare_train_input_field_query,
+            "function": train_input_text_field_query,
+            "args": [impira_api, args.org_id, collection_id, TABLE_TRAINER, args.gz]
+        }
     ]
 
     num_runs = 5
